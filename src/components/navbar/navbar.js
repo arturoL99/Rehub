14787102ded4de946720dc6fcce9a3d2978a4c84
Arturo Leone <arturo.leonesoprasteria.com>
2022-09-04 18:26:31 +0200

    refactored navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,27 +5,25 @@ import './navStyle.scss';
 
 export default function Navbar() {
 
-    const [isOpen = false, setOpen] = useState();
+    const [isOpen, setOpen] = useState(false);
 
-    const setMenu = (e) => {
-        console.log("try to open")
-        if (isOpen === false) {
-            document.querySelector(".nav").classList.replace("nav", "nav_open");
-            setOpen(!isOpen)
-        } else {
-            document.querySelector(".nav_open").classList.replace("nav_open", "nav");
-            setOpen(!isOpen)
-        }
-    }
-
+    const handleOpen = () => {
+        setOpen(!isOpen);
+    };
 
     return (
         <header>
             <div className="container">
-                <img src={logo} alt="logo re-hub" className="logo" />
-                <p className="mx-15 logo-title py-10">Re-Hub <br />Real Estate <br />Advisory & Agency</p>
+                <div className='logoContainer'>
+                    <img src={logo} alt="logo re-hub" className="logo" />
+                    <p className="mx-15 logo-title py-10">Re-Hub <br />Real Estate <br />Advisory & Agency</p>
+                </div>
+                <div className='menu'>
+                    <img src="https://img.icons8.com/ios/50/000000/menu--v1.png" alt='menu button'
+                        onClick={handleOpen} />
+                </div>
             </div>
-            <div className="nav">
+            <div className={isOpen ? "nav_open" : "nav"}>
                 <Link to={"/"} className="mx-15 py-20">Home</Link>
                 <Link to={"/servizi/utilizzatori"} className="mx-15 py-20">Servizi</Link>
                 {/* <div className='dropdown_content'>
@@ -38,9 +36,6 @@ export default function Navbar() {
                 <a href="#" className="mx-15 py-20">Contatti</a>
                 <Link to={"/join"} className="mx-15 py-20">Join us</Link>
                 <a href="#" className="mx-15 py-20">IT</a>
-            </div>
-            <div className='menu'>
-                <img src="https://img.icons8.com/ios/50/000000/menu--v1.png" alt='menu button' onClick={setMenu} />
             </div>
         </header>
     )
