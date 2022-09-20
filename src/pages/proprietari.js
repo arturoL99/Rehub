09cf055b62416830faa178bbafd2070f219813c4
Arturo Leone <arturo.leonesoprasteria.com>
2022-09-21 01:30:11 +0200

    modificato contact + nuove immagini

--- src/pages/proprietari.js
+++ src/pages/proprietari.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import Body from '../components/servizi/body-servizi';
 import { client } from "../client/client";
 import Loading from "../components/loading/loading";
+import { imgProprietari } from "../utils/ImageUtils";
 
 function Propietari(){
 
     const [servizi, setServizi] = useState();
+    const img = imgProprietari;
 
     useEffect(() => {
         client.getServizi()
@@ -15,7 +17,7 @@ function Propietari(){
     if(servizi){
     return(
         <>
-            <Body counter={2} servizi={servizi}/>
+            <Body counter={2} servizi={servizi} img={img} />
         </>        
     )
     }else{
--- src/pages/utilizzatori.js
+++ src/pages/utilizzatori.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import Body from '../components/servizi/body-servizi';
 import { client } from "../client/client";
 import Loading from "../components/loading/loading";
+import { imgUtilizzatori } from "../utils/ImageUtils";
 
 function Utilizzatori(){
 
     const [servizi, setServizi] = useState();
+    const img = imgUtilizzatori;
 
     useEffect(() => {
         client.getServizi()
@@ -15,7 +17,7 @@ function Utilizzatori(){
     if(servizi){
     return(
         <>
-            <Body counter={0} servizi={servizi}/>
+            <Body counter={0} servizi={servizi} img={img} />
         </>        
     )
     }else{
