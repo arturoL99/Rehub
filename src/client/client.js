050b9d036ea7e63665f60bb1c6722da6219f2602
Arturo Leone <arturo.leonesoprasteria.com>
2022-11-08 14:42:58 +0100

    aggiunto emailjs + fix .env

--- src/client/client.js
+++ src/client/client.js
@@ -1,8 +1,8 @@
 import { createClient } from "contentful";
 
 const contentfulClient = createClient({
-  space: `19923zao3hdd`,
-  accessToken: `gFX46UPB_S5FPS2kkhT-DRmrzOBblUWvhgN3eBX3XEA`,
+  space: `${process.env.REACT_APP_CONTENTFUL_SPACE}`,
+  accessToken: `${process.env.REACT_APP_CONTENTFUL_SECRET}`,
 });
 
 export default contentfulClient;
