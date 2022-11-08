d9eafbdb9a7906ad16aeefcdc2cc29a2de0e979a
Arturo Leone <arturo.leonesoprasteria.com>
2022-11-08 16:51:55 +0100

    refactored slider

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -19,17 +19,22 @@ export default function Slider(props) {
         else setCounter(immagini.length - 1);
     }
 
+    const imgStyles = {
+        backgroundImage: `url(${immagini[counter].fields.file.url})`
+    };
     if (!immagini) return <Loading />;
     return (
         <div className={open ? "slider_container_open" : "slider_container"}>
             <div className="slider_arrow_left">
                 <img src={leftArrow} onClick={indietro} className="slider_arrow" />
             </div>
-            <img src={immagini[counter].fields.file.url}
-                className={open ? "slider_img_open" : "slider_img"} onClick={() => setOpen(!open)} />
+            <div style={imgStyles} className="slide"
+                onClick={() => setOpen(!open)}></div>
+            {/* <img src={immagini[counter].fields.file.url}
+                className={open ? "slider_img_open" : "slider_img"} onClick={() => setOpen(!open)} /> */}
             <div className="slider_arrow_right">
                 <img src={rightArrow} onClick={avanti} className="slider_arrow" />
-                
+
             </div>
             <p className={open ? "slider_counter_open" : "slider_counter"}>{counter + 1}/{immagini.length}</p>
         </div>
