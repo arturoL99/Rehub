e292242dadb5660b575c8cd22ee67bcb87548dd7
Arturo Leone <arturo.leonesoprasteria.com>
2022-12-08 00:20:46 +0100

    added new policy + meta tags

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import contentfulClient from "../client/client";
+import Loading from "../components/loading/loading";
 import PrivacyPolicy from "../components/policies/privacy";
 
 function Privacy() {
+    const [privacy, setPrivacy] = useState();
+    useEffect(() => {
+        contentfulClient.getEntry('5hrlotzNnE7oRUYjqak4FV')
+            .then((entry) => setPrivacy(entry.fields.policy))
+            .catch(console.error)
+    }, []);
+    console.log(privacy);
+    if(privacy){
     return (
         <>
-            <PrivacyPolicy />
+            <PrivacyPolicy policy={privacy} />
         </>
     )
+    }else{
+        return(
+            <><Loading /></>
+        )
+    }
 }
 
 export default Privacy;
