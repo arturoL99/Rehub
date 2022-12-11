9090f57edc1a042616e6ad1a3b81b41a2cb87ac8
Arturo Leone <arturo.leonesoprasteria.com>
2022-12-11 19:19:38 +0100

    added viewport meta tag

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -18,8 +18,8 @@ export default function Footer() {
                         Tel.348 4414871 - P.IVA 11191330965 - N.REA MI2585603</small>
                     <small className="my-5">PEC: re-hub@pec.it</small>
                     <small className="my-5">Tutto il contenuto di questo sito è Copyright © RE-Hub Srl -
-                        <NavLink to={"/privacy"}>Privacy Policy</NavLink>
-                        - <NavLink to={"/cookies"}>Cookie Policy</NavLink></small>
+                        <NavLink to={"/privacy"}>Privacy Policy</NavLink></small>
+                        {/* - <NavLink to={"/cookies"}>Cookie Policy</NavLink> */}
                 </div>
             </div>
         </footer>
